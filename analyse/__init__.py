f2d2049aed0b615b1b3315746ba7c2d6dafaac49
chenglong <chenglong@imdada.cn>
2015-08-10 14:25:20 +0800

    [ADD] add __init__.py for lib,analyse,document

---
+++ analyse/__init__.py
@@ -0,0 +1,8 @@
+"""By using execfile(this_file, dict(__file__=this_file)) you will
+activate this virtualenv environment.
+
+This can be used when you must use an existing Python interpreter, not
+the virtualenv bin/python
+"""
+
+

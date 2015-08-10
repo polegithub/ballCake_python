7bf42e23de6ae718a2ed300055696794464d574d
chenglong <chenglong@imdada.cn>
2015-08-10 19:09:54 +0800

    each.a.string  增加\n

--- crawler/getBasicData/getBasicData.py
+++ crawler/getBasicData/getBasicData.py
@@ -18,7 +18,7 @@ class SearchData:
         f = open('data/howtoTucao.txt','w')     #打开文件#
         f.write("No Answer")
 
-        for pagenum in range(1,11):        #从第1页爬到第11页
+        for pagenum in range(1,5):        #从第1页爬到第11页
              strpagenum = str(pagenum)      #页数的str表示
              print "Getting data for Page " + strpagenum   #shell里面显示的，表示已爬到多少页
              url = "http://www.zhihu.com/collection/27109279?page="+strpagenum  #网址
@@ -31,8 +31,10 @@ class SearchData:
           #print type(each.string)
           #print each.name
             if each.name == 'h2' :      #如果Tag为h2类型，说明是问题
-                print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
-                if each.a.string:       #如果非空，才能写入
+                if (each.a and each.a.string) :       #如果非空，才能写入
+                    each.a.string = each.a.string + '\n'
+                    print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
+
                     f.write(each.a.string)
                 else :                  #否则写"No Answer"
                     f.write("No Answer")

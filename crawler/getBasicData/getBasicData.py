ac51d45878c3e52f19d025bfd056bbad199b5918
chenglong <chenglong@imdada.cn>
2015-08-11 18:21:25 +0800

    修改soup.findAll,采用新的解析方式,增加答案

--- crawler/getBasicData/getBasicData.py
+++ crawler/getBasicData/getBasicData.py
@@ -16,38 +16,57 @@ class SearchData:
     def search(self):
         print('start to search')
         f = open('data/howtoTucao.txt','w')     #打开文件#
-        f.write("No Answer")
 
-        for pagenum in range(1,5):        #从第1页爬到第11页
+        for pagenum in range(1,2):        #从第1页爬到第11页
              strpagenum = str(pagenum)      #页数的str表示
              print "Getting data for Page " + strpagenum   #shell里面显示的，表示已爬到多少页
              url = "http://www.zhihu.com/collection/27109279?page="+strpagenum  #网址
              page = urllib2.urlopen(url)     #打开网页
              soup = BeautifulSoup(page)      #用BeautifulSoup解析网页
+             # print(soup)
+             print('打印soup')
 
     #找到具有class属性为下面两个的所有Tag
-        ALL = soup.findAll(attrs = {'class' : ['zm-item-title','zh-summary summary clearfix'] })
+        # ALL = soup.findAll(attrs = {'class' : ['zm-item-title','zh-summary summary clearfix'] })
+        ALL = soup.findAll(attrs = {'class' : 'zm-item'})
+
+        number =1;
+
         for each in ALL :               #枚举所有的问题和回答
-          #print type(each.string)
-          #print each.name
-            if each.name == 'h2' :      #如果Tag为h2类型，说明是问题
-                #print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
-                if (each.a and each.a.string) :       #如果非空，才能写入
-                    each.a.string = each.a.string + '\n'
-                    print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
+            title = each.find(attrs = {'class':'zm-item-title'})
+            if title:
+                print('title:',title.text)
+                text = '\n' + str(number) +'.'+ title.text
+                f.write(text)
 
-                    f.write(each.a.string)
-                else :                  #否则写"No Answer"
-                    f.write("No Answer")
-            else :                      #如果是回答，同样写入
-                print each.string
+            answer = each.find(attrs = {'class':'zh-summary'})
 
-                if each.string:
-                    f.write(each.string)
-                else :
-                    f.write("No Answer")
+            if answer:
+                print('answer：', answer.text)
+                f.write(answer.text)
+
+            number = number+1
 
         f.close()                           #关闭文件
+
+            # if each.name == 'h2' :      #如果Tag为h2类型，说明是问题
+            #     #print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
+            #     if (each.a and each.a.string) :       #如果非空，才能写入
+            #         each.a.string = each.a.string + '\n'
+            #         # print each.a.string     #问题中还有一个<a..>，所以要each.a.string取出内容
+            #
+            #         f.write(each.a.string)
+            #     else :                  #否则写"No Answer"
+            #         f.write("No Answer+ '\n'")
+            # else :                      #如果是回答，同样写入
+            #     print each.string
+            #
+            #     if each.string:
+            #         print('答案：')
+            #         f.write(each.string)
+            #     else :
+            #         f.write("No Answer"+'\n')
+
 
 
 

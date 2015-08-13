4188254d7766b9db4e875ab22c557f8a7f51c55c
chenglong <chenglong@imdada.cn>
2015-08-13 19:49:54 +0800

    [ADD]增加数据库操作 dbHandle

---
+++ analyse/dbHandle.py
@@ -0,0 +1,50 @@
+__author__ = 'chengpoleness'
+
+import MySQLdb
+# -*- coding: utf-8 -*-
+
+#localhost = 127.0.0.1
+
+
+def do_init_db():
+
+    conn=MySQLdb.connect(host="localhost",user="root",passwd="",db="test",charset="utf8")
+    curs = conn.cursor()
+    #
+    # try:
+    #     curs.execute('create database sourceDataDB')
+    # except:
+    #     print('addBallCakeDB exist!')
+    #
+    #
+    # conn.select_db('sourceDataDB')
+
+    # create a table named addfields
+    # try:
+    #     curs.execute('create table addfields(id int PRIMARY KEY NOT NULL,name text)')
+    # except:
+    #     print('The table addfields exists!')
+
+
+    # add the fileds
+    # try:
+    #     for i in range(1):
+    #         sql = "alter table addfields add key%s text" %i
+    #         curs.execute(sql)
+    # except Exception,e:
+    #     print e
+
+
+    # for i in range(4): #insert 5 lines
+    #     sql = "insert into addfields set id=%s" %i
+    #     curs.execute(sql)
+    #     sql = "update addfields set name = 'hello%s' where id= %s"%(i,i)
+    #     curs.execute(sql)
+    #     # for j in range(5):
+    #     #     sql = "update addfields set key%s = 'world%s%s' where id=%s"%(j,i,j,i)
+    #     #     curs.execute(sql)
+
+    #this is very important
+    conn.commit()
+    curs.close()
+    conn.close()
--- run.py
+++ run.py
@@ -3,16 +3,35 @@ import urllib
 import re
 
 
+#备注： 这个暂时都导入，后续根据需求再更改
+
+from crawler.getBasicData.getBasicDataFromDouban import SearchData
 from crawler.getBasicData.getTianYaData import SearchData
-# from crawler.getBasicData.getZhihuData import SearchData
+from crawler.getBasicData.getZhihuData import SearchData
+from analyse.dbHandle import do_init_db
+
 
 
 print('Let us begin')
 
 if __name__=='__main__' :
     print('enter main')
-    f = SearchData()
-    f.search()
+
+    # 调试的时候根据需求更改：
+    # 调试爬虫 MainValue =1，
+    # 数据库  MainValue =2，
+    # 其他待定义
+
+    MainValue = 2
+
+    if MainValue == 1:
+        f = SearchData()
+        f.search()
+    elif MainValue == 2:
+        do_init_db()
+    else:
+        pass
+
 
 
 

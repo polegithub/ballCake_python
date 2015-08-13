71f5648ee96887ea8af1be50e47e8a78a4e2debf
chenglong <chenglong@imdada.cn>
2015-08-14 01:47:51 +0800

    [REV] 数据库增加douban_book 及相对应的key

--- analyse/dbHandle.py
+++ analyse/dbHandle.py
@@ -11,34 +11,41 @@ import MySQLdb
 
 def do_init_db():
 
-    conn=MySQLdb.connect(host="127.0.0.1",user="root",passwd="",db="ballCake",charset="utf8")
+    conn=MySQLdb.connect(host="127.0.0.1",user="root",passwd="",db="mysql",charset="utf8")
     curs = conn.cursor()
 
-    # try:
-    #     curs.execute('create database sourceDataDB')
-    # except:
-    #     print('addBallCakeDB success!')
-    #
-    #
-    # conn.select_db('sourceDataDB')
-    #
-    # # n = curs.execute("select * from user")
-    # # for row in curs.fetchall():
-    # #     print row
-    # #     for r in row:
-    # #         print r
-    #
-    # # create a table named addfields
-    # try:
-    #     curs.execute('create table addfields(id int PRIMARY KEY NOT NULL,name text)')
-    # except:
-    #     print('The table addfields success!')
-    #
-    #
-    # # add the fileds
+    try:
+        curs.execute('create database if not exist ballCake')
+    except:
+        print('addBallCakeDB success!')
+
+
+    conn.select_db('ballCake')
+
+    # n = curs.execute("select * from user")
+    # for row in curs.fetchall():
+    #     print row
+    #     for r in row:
+    #         print r
+
+    # create a table named addfields
+    try:
+        curs.execute('create table douban_book(id int PRIMARY KEY NOT NULL,bookName text,score text,scoedNum int)')
+    except:
+        print('The table douban_book success!')
+
+    try:
+        # sql = "alter table douban_book add (publisher text,author text,publishTime text,price text)"
+        sql = "alter table douban_book add (price text)"
+        curs.execute(sql)
+    except:
+        print('add keys for douban')
+
+
+    # add the fileds
     # try:
     #     for i in range(1):
-    #         sql = "alter table addfields add key%s text" %i
+    #         sql = "alter table douba_book add key%s text" %i
     #         curs.execute(sql)
     # except Exception,e:
     #     print e

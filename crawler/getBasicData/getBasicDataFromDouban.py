72cfee4c33dbf875a1b252119cec70eda9c790aa
chenglong <chenglong@imdada.cn>
2015-08-21 10:22:56 +0800

    modify douban.py

--- crawler/getBasicData/getBasicDataFromDouban.py
+++ crawler/getBasicData/getBasicDataFromDouban.py
@@ -10,27 +10,28 @@ sys.setdefaultencoding("utf-8")
 class SearchData:
 
     def __init__(self):
-	    print "SearchData-init"
-	    pass
-		
+        print "SearchData-init"
+        pass
+
     def search(self):
-	    print "start to search"
-	    f = open('data/getdatafromdb.txt', 'w')
-		
-	    url = "http://movie.douban.com/subject/26277313/"
-	    page = urllib2.urlopen(url)
-	    soup = BeautifulSoup(page)
-		
-	    ALL = soup.findAll(attrs = {'class': 'review-short'})
-		
-	    number = 1
-	
-	    for each in ALL:
-		    #title = each.find("a", attrs = {"title": True})
-		    if each.span.string:
-			    text = '\n' + str(number) + "." + each.span.string
-			    f.write(text)
-				
-		    number += 1
-				
-	    f.close()
+        print "start to search"
+        f = open('data/getdatafromdb.txt', 'w')
+
+        url = "http://movie.douban.com/subject/26277313/"
+        page = urllib2.urlopen(url)
+        soup = BeautifulSoup(page)
+
+        ALL = soup.findAll(attrs = {'class': 'review-short'})
+
+        number = 1
+
+        for each in ALL:
+            #title = each.find("a", attrs = {"title": True})
+            if each.span.string:
+                text = '\n' + str(number) + "." + each.span.string
+                f.write(text)
+
+            number += 1
+
+        f.close()
+
